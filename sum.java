2ec9fcc31ed2dcc602afa7902d0536df0bc916ea
Jaskaran Singh <jaskaranmain@gmail.com>
2024-03-23 01:59:40 +0530

    commming all files

--- sum.java
+++ sum.java
@@ -1,6 +1,7 @@
+package LearnGit;
 public class sum{
     public static void main(String[] args) {
         int a = 2,b = 3;
-        System.out.println(a+b);
+        System.out.println("the sum of a and b is "+a+b);
     }
 }
